0ddaad9808253c67f33a75b9f7c612071d294782
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-11-02 00:49:08 +0500

    added cors

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 require("dotenv/config");
 
 const app = express();
 const PORT = process.env.PORT; 
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
