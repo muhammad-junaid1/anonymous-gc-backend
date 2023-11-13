b0fb0807430fbbb7c6eac68f6d53fc58aa7789f9
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-11-13 23:53:30 +0500

    settings done

--- app.js
+++ app.js
@@ -14,12 +14,12 @@ const PORT = process.env.PORT || 5000;
 
 const indexRouter = require('./routes/index');
 
+app.use("/images", express.static(path.join(__dirname, './files')));
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 
 // Auth routes
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,62 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 const User = require("../models/User");
+const multer = require("multer");
+const path = require("path");
+
+class UserData {
+  constructor(data){
+    this.username = data?.username; 
+    this.role = data?.role; 
+    this.profile_picture = data?.profile_picture;
+  }
+}
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "files/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, req.user + "_" + file.originalname?.toString()?.replaceAll(" ", ""));
+  },
+});
+
+const upload = multer({ storage });
 
 router.get("/profile", async (req, res) => {
-  const user = await User.findOne({_id: req.user});
+  const user = await User.findOne({ _id: req.user });
+
+  const data = new UserData(user);
+  res.json({
+    status: true,
+    data,
+  });
+});
+
+router.post("/profile", async (req, res) => {
+  await User.findOneAndUpdate({_id: req.user}, {
+    username: req.body.username
+  });
+
+  const updated = await User.findOne({_id: req.user});
+
+  const data = new UserData(updated);
   res.json({
     status: true, 
-    data: user 
-  })
+    message: "User updated successfuly", 
+    data
+  });
 })
 
+router.post("/upload-file", upload.single("file"), async (req, res) => {
+  const uploadedFile = req.file; 
+  const fileName = uploadedFile.filename?.toString()?.replaceAll(" ", ""); 
+  const fileURL = `${req.get('x-forwarded-proto') || req.protocol}://${req.get('x-forwarded-host') || req.get('host')}/images/${fileName}`;
+
+  await User.findOneAndUpdate({_id: req.user}, {
+    profile_picture: fileURL
+  });
+  res.json();
+});
 
 module.exports = router;
