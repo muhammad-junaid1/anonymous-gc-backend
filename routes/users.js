599edc96aaa44a65db88ad8c44a5a906f6fe08c3
Muhammad Junaid <mjunaid.swe@gmail.com>
2023-11-27 05:33:45 +0500

    update employee done

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const path = require("path");
 
 router.get("/", async (req, res) => {
   try {
-    const allUsers = await User.find({ role: 2 }).sort({createdAt: -1});
+    const allUsers = await User.find({ role: 2 }).sort({ createdAt: -1 });
     res.json({
       status: true,
       data: allUsers,
@@ -44,7 +44,60 @@ router.post("/", upload.single("file"), async (req, res) => {
     await newUser.save();
     res.json({
       status: true,
-      message: "The User is created successfuly",
+      message: "The employee is created successfuly",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      status: false,
+      message: "Something went wrong",
+    });
+  }
+});
+
+router.post("/:userID", upload.single("file"), async (req, res) => {
+  const uploadedFile = req.file;
+  let fileURL = "";
+
+  if (uploadedFile) {
+    const fileName = uploadedFile?.filename?.toString()?.replaceAll(" ", "");
+    fileURL = `${req.get("x-forwarded-proto") || req.protocol}://${
+      req.get("x-forwarded-host") || req.get("host")
+    }/images/${fileName}`;
+  }
+
+  const updatedUser = {
+    displayName: req.body.displayName,
+    username: req.body.username,
+    password: req.body.password,
+  };
+
+  if (fileURL) {
+    updatedUser["profile_picture"] = fileURL;
+  }
+
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.params?.userID },
+      updatedUser
+    );
+    if (fileURL) {
+      fs.unlink(
+        path.join(
+          __dirname,
+          "../files/" +
+            user?.profile_picture?.slice(
+              user?.profile_picture.indexOf("images/") + 7
+            )
+        ),
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+    res.json({
+      status: true,
+      message: "The employee is updated successfuly",
     });
   } catch (error) {
     console.log(error);
@@ -58,16 +111,6 @@ router.post("/", upload.single("file"), async (req, res) => {
 router.delete("/:userID", async (req, res) => {
   try {
     const user = await User.findOneAndDelete({ _id: req.params.userID });
-    console.log(user);
-    console.log(
-      path.join(
-        __dirname,
-        "../files/" +
-          user?.profile_picture?.slice(
-            user?.profile_picture.indexOf("images/") + 7
-          )
-      )
-    );
     if (user?.profile_picture) {
       fs.unlink(
         path.join(
